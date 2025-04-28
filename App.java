84032fc49f1b8a919a8ac0e2d25f6fe941e800b7
Chethangowdakn <chethankn.22cse@cambridge.edu.in>
2025-04-28 11:53:09 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package cse.cambridge3.prog4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World i am mandy! " );
+    }
+}
